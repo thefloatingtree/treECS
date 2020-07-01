5a4e51eef3d529625f9852aa79eea80b35fc955b
thefloatingtree <thefloatingtree@gmail.com>
2020-06-30 23:56:19 -0600

    added init to system

--- dist/index.js
+++ dist/index.js
@@ -902,6 +902,9 @@ class System {
     constructor(entityAdmin, queries) {
         this.queries = queries;
         this.entityAdmin = entityAdmin;
+
+        if (this.init)
+            this.init();
     }
 }
 
--- src/System.js
+++ src/System.js
@@ -2,5 +2,8 @@ export class System {
     constructor(entityAdmin, queries) {
         this.queries = queries;
         this.entityAdmin = entityAdmin;
+
+        if (this.init)
+            this.init();
     }
 }
